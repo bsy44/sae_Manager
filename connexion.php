7331fd780d09c72d74f1a409adf4ed44aa33f6fa
Hamidur RAHMAN <123751122+rudimah@users.noreply.github.com>
2024-12-12 11:52:59 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,15 @@
+<?php
+
+Class connexion{
+    
+    protected static $bdd;
+    
+    public static function initconnexion(){
+        $dsn = 'mysql:dbname=dutinfopw201642;host=database-etudiants.iut.univ-paris8.fr';
+        $user = 'dutinfopw201642';
+        $password = 'pubepete';
+
+        self::$bdd = new PDO($dsn, $user, $password);
+    }
+}
+?>
