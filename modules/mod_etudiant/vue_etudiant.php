dd883f9732fcaab6b7ca31b1d6cb7a207fd29750
bsy44 <syboubacar444@gmail.com>
2025-01-09 00:31:26 +0100

    commit page minimaliste connexion

--- modules/mod_etudiant/vue_etudiant.php
+++ modules/mod_etudiant/vue_etudiant.php
@@ -65,61 +65,13 @@ class vue_etudiant {
                 <button type="button" id="ajouterEleve">Ajouter élève</button>
                 <input type="submit" value="Proposer le groupe" id="envoyerGroupe">
             </form>
-            <button id="close">Fermer</button>
         </div> 
         
         <script>
-            let boutonClose = document.getElementById("close")
-            boutonClose.addEventListener("click", () => {
-                document.getElementById("popUpGrp").style.display = "none"
-            })
-        
-            let form = document.getElementById("formGroupe")
-            let bouttonEleve = document.getElementById("ajouterEleve")
             let optionsHTML = `' . addslashes($optionsPrenom) . '`
-            let nbEleve = 1
-        
-            bouttonEleve.addEventListener("click", () => {
-                if (nbEleve < 4) {
-                    nbEleve++
-                    let uniqueId = `nom${nbEleve}`
-                    let option = document.createElement("div")
-                    option.className = "form-option"
-                    option.innerHTML = `
-                        <label for="${uniqueId}">Étudiant ${nbEleve} :</label>
-                        <select name="idEtu${nbEleve}">
-                            ${optionsHTML}
-                        </select>
-                    `
-                    let boutonRetirer = document.createElement("button")
-                    boutonRetirer.type = "button"
-                    boutonRetirer.innerText = "X"
-                    boutonRetirer.addEventListener("click", () => {
-                        form.removeChild(option)
-                        nbEleve--
-                        updateLabels()
-                    })
-                    option.appendChild(boutonRetirer)
-                    form.insertBefore(option, bouttonEleve)
-                }
-            })
-        
-            function updateLabels() {
-                let optionsPrenom = form.querySelectorAll(".form-option")
-                let index = 1
-                optionsPrenom.forEach(option => {
-                    let label = option.querySelector("label")
-                    let select = option.querySelector("select")
-                    let newId = `nom${index}`
-                    label.innerText = `Étudiant ${index} :`
-                    label.setAttribute("for", newId)
-                    select.id = newId
-                    index++
-                })
-            }
         </script>
+
         ';
     }
-    
 }
 ?>
